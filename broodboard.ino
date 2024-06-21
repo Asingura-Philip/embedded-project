b539fb2b45f56abfe4d4600de53b26540ae56bb5
Asingura Philip <asinguraphilip@gmail.com>
2024-06-21 10:05:56 +0300

    added servo motor functionality

--- broodboard.ino
+++ broodboard.ino
@@ -6,6 +6,12 @@
 #define LED1_PIN 6           // Digital pin for LED 1
 #define LED2_PIN 7           // Digital pin for LED 2
 #define LED3_PIN 8           // Digital pin for LED 3
+#define SERVO_PIN 9          // PWM pin for servo motor
+
+
+
+Servo servoMotor;
+
 void setup() {
     Serial.begin(9600);  // Initialize serial communication
     lcd_1.begin(16, 2);   // Set up the number of columns and rows on the LCD.
@@ -14,6 +20,8 @@ void setup() {
     pinMode(LED1_PIN, OUTPUT);
     pinMode(LED2_PIN, OUTPUT);
     pinMode(LED3_PIN, OUTPUT);
+
+    servoMotor.attach(SERVO_PIN);
 
 
 }
